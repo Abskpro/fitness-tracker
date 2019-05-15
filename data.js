a84aee621242288e3e522952badab866fe74ac60
Absk <karma1997.acharya@gmail.com>
2019-05-15 18:07:54 +0545

    Changed Port

--- data.js
+++ data.js
@@ -1,9 +1,13 @@
+var PORT = process.env.PORT || 5000;
 const aps = require('./script.js');
 /*var obj = {
        table: []
 
 };
 obj.table.push({id: 1, square:2});*/
+server.listen(PORT,function(){
+    console.log("chat server running");
+})
 var json = JSON.stringify(aps);
 var fs = require('fs');
 fs.writeFile('myjsonfile.json', json, 'utf8', function(err){
